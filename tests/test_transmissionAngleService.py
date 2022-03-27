067f01fd540abd95a93c44b22e40c194e1864b1b
george <george.duffett@icloud.com>
2022-03-27 12:50:51 +0100

    Test propagating transmission angles

--- tests/test_transmissionAngleService.py
+++ tests/test_transmissionAngleService.py
@@ -32,6 +32,28 @@ class transmissionAngles(unittest.TestCase):
         # Then
         self.assertAlmostEqual(transmissionAngle, np.pi / 2)
 
+    def test_propagateTransmissionAngles_symmetricStack(self):
+        # Given
+        coverRefractiveIndex = 2
+        substrateRefractiveIndices = [
+            (coverRefractiveIndex, np.sqrt(2)),
+            (np.sqrt(2), coverRefractiveIndex),
+        ]
+        incidentAngle = np.deg2rad(45)
+
+        # When
+        transmissionAngles = ta.propagateTransmissionAngles(
+            incidentAngle, substrateRefractiveIndices
+        )
+
+        # Then
+        diff = sum(
+            (angle1 - angle2) ** 2
+            for angle1, angle2 in zip(transmissionAngles, [np.pi / 2, incidentAngle])
+        )
+        self.assertAlmostEqual(diff, 0)
+        self.assertAlmostEqual(transmissionAngles[-1], incidentAngle)
+
 
 if __name__ == "__main__":
     unittest.main()
