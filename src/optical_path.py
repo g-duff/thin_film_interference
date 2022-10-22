fbd5836e7f5b33e45a45a59612f2be10db53b241
george <george.duffett@icloud.com>
2022-10-22 17:35:51 +0100

    Change optical_path formatting

--- src/optical_path.py
+++ src/optical_path.py
@@ -1,6 +1,7 @@
 '''Optical propagation'''
 from numpy import cos
 
+
 def accumulate_phase(wavenumber, ray_angle, thickness):
     '''Phase accumulated along path'''
     optical_path_length = thickness * cos(ray_angle) * wavenumber
