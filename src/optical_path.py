933fb917213d3236f77fe1e6a0741d8fcc35883c
george <george.duffett@icloud.com>
2022-10-22 23:26:01 +0100

    Change phase interface

--- src/optical_path.py
+++ src/optical_path.py
@@ -4,5 +4,4 @@ from numpy import cos
 
 def accumulate_phase(wavenumber, ray_angle, thickness):
     '''Phase accumulated along path'''
-    optical_path_length = thickness * cos(ray_angle) * wavenumber
-    return optical_path_length
+    return 2 * thickness * cos(ray_angle) * wavenumber
