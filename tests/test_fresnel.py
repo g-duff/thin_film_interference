071cd9faad511ca91a8ac4772c7786cdcc92a0bf
george <george.duffett@icloud.com>
2022-10-22 13:28:42 +0100

    Change fresnel test function, variable names

--- tests/test_fresnel.py
+++ tests/test_fresnel.py
@@ -1,71 +1,71 @@
 # pylint: disable = import-error, missing-class-docstring, missing-function-docstring, missing-module-docstring
-import numpy as np
-import unittest
 from src.fresnel import Parallel, Senkrecht
 import src.transmission_angles as ta
+import numpy as np
+import unittest
 
 
 class Fresnel(unittest.TestCase):
-    def test_Senkrecht_energyconservation(self):
+    def test_senkrecht_energyconservation(self):
 
         # Given
-        incidentAngle = 15
-        coverRefractiveIndex = 1.0
-        substrateRefractiveIndex = 1.5
-        transmissionAngle = ta.calculate_transmission_angle(
-            coverRefractiveIndex, substrateRefractiveIndex, incidentAngle
+        incident_angle = 15
+        cover_refractive_index = 1.0
+        substrate_refractive_index = 1.5
+        transmission_angle = ta.calculate_transmission_angle(
+            cover_refractive_index, substrate_refractive_index, incident_angle
         )
 
         # When
         reflection = Senkrecht.reflection(
-            incidentAngle,
-            transmissionAngle,
+            incident_angle,
+            transmission_angle,
         )
         transmission = Senkrecht.transmission(
-            incidentAngle,
-            transmissionAngle,
+            incident_angle,
+            transmission_angle,
         )
 
         # Then
         reflectivity = reflection**2
         transmissivity = (
             transmission**2
-            * substrateRefractiveIndex
-            * np.cos(transmissionAngle)
-            / coverRefractiveIndex
-            / np.cos(incidentAngle)
+            * substrate_refractive_index
+            * np.cos(transmission_angle)
+            / cover_refractive_index
+            / np.cos(incident_angle)
         )
         energy = reflectivity + transmissivity
         self.assertAlmostEqual(energy, 1)
 
-    def test_Parallel_energyconservation(self):
+    def test_parallel_energyconservation(self):
 
         # Given
-        incidentAngle = 15
-        coverRefractiveIndex = 1.0
-        substrateRefrativeIndex = 1.5
-        transmissionAngle = ta.calculate_transmission_angle(
-            coverRefractiveIndex, substrateRefrativeIndex, incidentAngle
+        incident_angle = 15
+        cover_refractive_index = 1.0
+        substrate_refrative_index = 1.5
+        transmission_angle = ta.calculate_transmission_angle(
+            cover_refractive_index, substrate_refrative_index, incident_angle
         )
 
         # When
         reflection = Parallel.reflection(
-            incidentAngle,
-            transmissionAngle,
+            incident_angle,
+            transmission_angle,
         )
         transmission = Parallel.transmission(
-            incidentAngle,
-            transmissionAngle,
+            incident_angle,
+            transmission_angle,
         )
 
         # Then
         reflectivity = reflection**2
         transmissivity = (
             transmission**2
-            * substrateRefrativeIndex
-            * np.cos(transmissionAngle)
-            / coverRefractiveIndex
-            / np.cos(incidentAngle)
+            * substrate_refrative_index
+            * np.cos(transmission_angle)
+            / cover_refractive_index
+            / np.cos(incident_angle)
         )
         test_energy = reflectivity + transmissivity
         self.assertAlmostEqual(test_energy, 1)
