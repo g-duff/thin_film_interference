f885b125d67ad46f85b6daa60379295ddef938bd
george <george.duffett@icloud.com>
2022-10-22 12:18:50 +0100

    Add optical boundary domain doc strings

--- src/opticalInterfaceDomain.py
+++ src/opticalInterfaceDomain.py
@@ -1,22 +1,29 @@
+'''Optical Interface calculations'''
+
 class OpticalInterface:
+    '''Optical Interface class, encapsulating Fresnel coefficients'''
     def __init__(self, refractiveIndexPair, rayAnglePair):
         self.refractiveIndexPair = refractiveIndexPair
         self.rayAnglePair = rayAnglePair
 
     def setPolarization(self, Polarization):
+        '''Set polarization for reflection and transmission calculations'''
         self.Polarization = Polarization
 
     def reflectionInto(self):
+        '''Calculate reflection towards the interface'''
         return self.Polarization.reflection(
             *self.rayAnglePair,
         )
 
     def transmissionInto(self):
+        '''Calculate transmission through the interface'''
         return self.Polarization.transmission(
             *self.rayAnglePair,
         )
 
     def transmissionBack(self):
+        '''Calculate transmission back through interface'''
         return self.Polarization.transmission(
             *self.rayAnglePair[::-1],
         )
