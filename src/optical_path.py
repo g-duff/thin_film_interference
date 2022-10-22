58ca5b23e8d9a9195b051d03992da44642509fd4
george <george.duffett@icloud.com>
2022-10-22 12:40:57 +0100

    Add optical path doc strings

--- src/optical_path.py
+++ src/optical_path.py
@@ -1,13 +1,16 @@
+'''Optical propagation'''
 from numpy import cos
 
 
 class OpticalPath:
+    '''Path along which light propagates'''
     def __init__(self, refractive_index, thickness, ray_angle):
         self.refractive_index = refractive_index
         self.thickness = thickness
         self.ray_angle = ray_angle
 
     def accumulatePhase(self, free_space_wavenumber):
+        '''Phase accumulated along path'''
         optical_thickness = self.refractive_index * self.thickness
         optical_path_length = 2 * optical_thickness * cos(self.ray_angle)
         return free_space_wavenumber * optical_path_length
