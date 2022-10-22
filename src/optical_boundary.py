bdcddab0aa475866ee7dd53c02c6d7a19ff62736
george <george.duffett@icloud.com>
2022-10-22 12:33:59 +0100

    Change optical boundary fresnel coefficient calls

--- src/optical_boundary.py
+++ src/optical_boundary.py
@@ -1,9 +1,12 @@
 '''Optical Interface calculations'''
 
+
 class OpticalBoundary:
     '''Optical Interface class, encapsulating Fresnel coefficients'''
+
     def __init__(self, incident_ray_angle, transmitted_ray_angle):
-        self.rayAnglePair = (incident_ray_angle, transmitted_ray_angle)
+        self.incident_ray_angle = incident_ray_angle
+        self.transmitted_ray_angle = transmitted_ray_angle
 
     def set_polarization(self, Polarization):
         '''Set polarization for reflection and transmission calculations'''
@@ -12,17 +15,20 @@ class OpticalBoundary:
     def reflection_into(self):
         '''Calculate reflection towards the interface'''
         return self.Polarization.reflection(
-            *self.rayAnglePair,
+            self.incident_ray_angle,
+            self.transmitted_ray_angle
         )
 
     def transmission_into(self):
         '''Calculate transmission through the interface'''
         return self.Polarization.transmission(
-            *self.rayAnglePair,
+            self.incident_ray_angle,
+            self.transmitted_ray_angle
         )
 
     def transmission_back(self):
         '''Calculate transmission back through interface'''
         return self.Polarization.transmission(
-            *self.rayAnglePair[::-1],
+            self.transmitted_ray_angle,
+            self.incident_ray_angle
         )
