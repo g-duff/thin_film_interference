4085f8a52a93b892f34c79daae10464589175343
george <george.duffett@icloud.com>
2022-10-22 13:23:49 +0100

    Change transmission test function, variable names

--- tests/test_transmission_angles.py
+++ tests/test_transmission_angles.py
@@ -4,56 +4,56 @@ import numpy as np
 import src.transmission_angles as ta
 
 
-class transmissionAngles(unittest.TestCase):
-    def test_calculateAngleOfTransmission_normalIncidence(self):
+class TransmissionAngles(unittest.TestCase):
+    def test_transmission_angle_normal_incidence(self):
         # Given
-        coverRefractiveIndex = 1.0
-        substrateRefractiveIndex = 1.5
-        incidentAngle = 0
+        cover_refractive_index = 1.0
+        substrate_refractive_index = 1.5
+        incident_angle = 0
 
         # When
-        transmissionAngle = ta.calculate_transmission_angle(
-            coverRefractiveIndex, substrateRefractiveIndex, incidentAngle
+        transmission_angle = ta.calculate_transmission_angle(
+            cover_refractive_index, substrate_refractive_index, incident_angle
         )
 
         # Then
-        self.assertEqual(transmissionAngle, 0)
+        self.assertEqual(transmission_angle, 0)
 
-    def test_calculateAngleOfTransmission_obliqueIncidence(self):
+    def test_calculate_transmission_angle_oblique_incidence(self):
         # Given
-        coverRefractiveIndex = 2
-        substrateRefractiveIndex = np.sqrt(2)
-        incidentAngle = np.deg2rad(45)
+        cover_refractive_index = 2
+        substrate_refractive_index = np.sqrt(2)
+        incident_angle = np.deg2rad(45)
 
         # When
-        transmissionAngle = ta.calculate_transmission_angle(
-            coverRefractiveIndex, substrateRefractiveIndex, incidentAngle
+        transmission_angle = ta.calculate_transmission_angle(
+            cover_refractive_index, substrate_refractive_index, incident_angle
         )
 
         # Then
-        self.assertAlmostEqual(transmissionAngle, np.pi / 2)
+        self.assertAlmostEqual(transmission_angle, np.pi / 2)
 
-    def test_propagateTransmissionAngles_symmetricStack(self):
+    def test_propagatetransmission_angles_symmetric_stack(self):
         # Given
-        coverRefractiveIndex = 2
-        substrateRefractiveIndices = [
-            (coverRefractiveIndex, np.sqrt(2)),
-            (np.sqrt(2), coverRefractiveIndex),
+        cover_refractive_index = 2
+        substrate_refractive_indices = [
+            (cover_refractive_index, np.sqrt(2)),
+            (np.sqrt(2), cover_refractive_index),
         ]
-        incidentAngle = np.deg2rad(45)
+        incident_angle = np.deg2rad(45)
 
         # When
-        transmissionAngles = ta.propagate_transmission_angles(
-            incidentAngle, substrateRefractiveIndices
+        transmission_angles = ta.propagate_transmission_angles(
+            incident_angle, substrate_refractive_indices
         )
 
         # Then
         diff = sum(
             (angle1 - angle2) ** 2
-            for angle1, angle2 in zip(transmissionAngles, [np.pi / 2, incidentAngle])
+            for angle1, angle2 in zip(transmission_angles, [np.pi / 2, incident_angle])
         )
         self.assertAlmostEqual(diff, 0)
-        self.assertAlmostEqual(transmissionAngles[-1], incidentAngle)
+        self.assertAlmostEqual(transmission_angles[-1], incident_angle)
 
 
 if __name__ == "__main__":
