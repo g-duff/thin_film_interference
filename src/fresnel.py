21d302617a8a6921c009d203623582443cfbf2a9
george <george.duffett@icloud.com>
2022-10-24 00:55:49 +0100

    Change Senkrecht to take wavevector components

--- src/fresnel.py
+++ src/fresnel.py
@@ -7,22 +7,25 @@ class Senkrecht:
     '''Senkrecht, or Perpendicular polarization'''
     @staticmethod
     def reflection(
-        incident_angle,
-        transmission_angle,
+        incident_wavevector_normal_component,
+        transmission_wavevector_normal_component,
     ):
         '''Calculate reflection coefficient'''
-        numerator = -1 * np.sin(incident_angle - transmission_angle)
-        denominator = np.sin(incident_angle + transmission_angle)
+        numerator = incident_wavevector_normal_component - \
+            transmission_wavevector_normal_component
+        denominator = incident_wavevector_normal_component + \
+            transmission_wavevector_normal_component
         return numerator / denominator
 
     @staticmethod
     def transmission(
-        incident_angle,
-        transmission_angle,
+        incident_wavevector_normal_component,
+        transmission_wavevector_normal_component,
     ):
         '''Calculate transmission coefficient'''
-        numerator = 2 * np.sin(transmission_angle) * np.cos(incident_angle)
-        denominator = np.sin(incident_angle + transmission_angle)
+        numerator = 2 * incident_wavevector_normal_component
+        denominator = incident_wavevector_normal_component + \
+            transmission_wavevector_normal_component
         return numerator / denominator
 
 
--- tests/test_fresnel.py
+++ tests/test_fresnel.py
@@ -9,34 +9,30 @@ class Fresnel(unittest.TestCase):
     def test_senkrecht_energyconservation(self):
 
         # Given
-        incident_angle = 15
+        incident_angle = np.deg2rad(15)
         cover_refractive_index = 1.0
         substrate_refractive_index = 1.5
         transmission_angle = ta.calculate_transmission_angle(
             cover_refractive_index, substrate_refractive_index, incident_angle
         )
 
+        incident_wavevector_normal_component = cover_refractive_index * \
+            np.cos(incident_angle)
+        transmission_wavevector_normal_component = substrate_refractive_index * \
+            np.cos(transmission_angle)
+
         # When
         reflection = Senkrecht.reflection(
-            incident_angle,
-            transmission_angle,
+            incident_wavevector_normal_component,
+            transmission_wavevector_normal_component,
         )
         transmission = Senkrecht.transmission(
-            incident_angle,
-            transmission_angle,
+            incident_wavevector_normal_component,
+            transmission_wavevector_normal_component,
         )
 
         # Then
-        reflectivity = reflection**2
-        transmissivity = (
-            transmission**2
-            * substrate_refractive_index
-            * np.cos(transmission_angle)
-            / cover_refractive_index
-            / np.cos(incident_angle)
-        )
-        energy = reflectivity + transmissivity
-        self.assertAlmostEqual(energy, 1)
+        self.assertAlmostEqual(transmission, reflection + 1)
 
     def test_parallel_energyconservation(self):
 
