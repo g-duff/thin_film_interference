f6b138b6aa2d1fac9df49a2412c4225a0ebc9214
george <george.duffett@icloud.com>
2022-10-24 23:44:43 +0100

    Remove optical path module

--- src/optical_path.py
+++
@@ -1,9 +0,0 @@
-'''Optical propagation'''
-import numpy as np
-
-
-def accumulate_phase(free_space_wavenumber, ray_angle, refractive_index, thickness):
-    '''Phase accumulated along path'''
-    optical_path_difference = 2 * refractive_index * \
-        thickness * np.cos(ray_angle)
-    return optical_path_difference * free_space_wavenumber
