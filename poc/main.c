830918717c4737c346bac75b05f1e222874ff1c5
george <george.duffett@icloud.com>
2022-10-30 10:13:28 +0000

    Change parallel reflection to use pointers

--- poc/main.c
+++ poc/main.c
@@ -22,15 +22,15 @@ float complex calculate_senkrecht_transmission (
 }
 
 float complex calculate_parallel_reflection(
-        float complex incident_wavevector_normal_component,
-        float complex transmission_wavevector_normal_component,
-        float complex incident_refractive_index,
-        float complex transmission_refractive_index
+        float complex *incident_wavevector_normal_component,
+        float complex *transmission_wavevector_normal_component,
+        const float complex *incident_refractive_index,
+        const float complex *transmission_refractive_index
 ) {
-        float complex numerator = incident_wavevector_normal_component * pow(transmission_refractive_index,2) - 
-            transmission_wavevector_normal_component * pow(incident_refractive_index,2);
-        float complex denominator = incident_wavevector_normal_component * pow(transmission_refractive_index,2) + 
-            transmission_wavevector_normal_component * pow(incident_refractive_index,2);
+        float complex numerator = (*incident_wavevector_normal_component) * pow(*transmission_refractive_index,2) - 
+            (*transmission_wavevector_normal_component) * pow(*incident_refractive_index,2);
+        float complex denominator = (*incident_wavevector_normal_component) * pow(*transmission_refractive_index,2) + 
+            *transmission_wavevector_normal_component * pow(*incident_refractive_index,2);
         return numerator / denominator;
 }
 
@@ -64,7 +64,7 @@ int main (void) {
     const float thicknesses[number_of_films] = {220, 3000};
     
     const int number_of_layers = number_of_films+2;
-    const float refractive_indexes[number_of_layers] = {1.0, 3.8, 1.45, 3.8};
+    const float complex refractive_indexes[number_of_layers] = {1.0, 3.8, 1.45, 3.8};
 
     const float wavelength = 500.056;
     const float incident_angle = 65.0 * M_PI /180.0;
@@ -80,17 +80,17 @@ int main (void) {
     float complex senkrecht_reflection = calculate_senkrecht_reflection(
         &wavevector_normal_components[number_of_layers-2], &wavevector_normal_components[number_of_layers-1]);
     float complex parallel_reflection = calculate_parallel_reflection(
-        wavevector_normal_components[number_of_layers-2], wavevector_normal_components[number_of_layers-1],
-        refractive_indexes[number_of_layers-2], refractive_indexes[number_of_layers-1]);
+        &wavevector_normal_components[number_of_layers-2], &wavevector_normal_components[number_of_layers-1],
+        &refractive_indexes[number_of_layers-2], &refractive_indexes[number_of_layers-1]);
 
     for (int i=number_of_films; i>0; i--) {
 
         const float *film_thickness = &thicknesses[i-1];
 
-        float film_refractive_index = refractive_indexes[i];
+        const float complex film_refractive_index = refractive_indexes[i];
         float complex film_wavevector_normal_component = wavevector_normal_components[i];
 
-        float incident_refractive_index = refractive_indexes[i-1];
+        const float complex incident_refractive_index = refractive_indexes[i-1];
         float complex incident_wavevector_normal_component = wavevector_normal_components[i-1];
 
         float complex accumulated_phase = 2 * (*film_thickness) * film_wavevector_normal_component;
@@ -98,8 +98,8 @@ int main (void) {
         parallel_reflection = calculate_film_reflection(
             parallel_reflection,
             calculate_parallel_reflection(
-                incident_wavevector_normal_component, film_wavevector_normal_component,
-                incident_refractive_index, film_refractive_index),
+                &incident_wavevector_normal_component, &film_wavevector_normal_component,
+                &incident_refractive_index, &film_refractive_index),
             calculate_parallel_transmission(
                 incident_wavevector_normal_component, film_wavevector_normal_component,
                 incident_refractive_index, film_refractive_index),
