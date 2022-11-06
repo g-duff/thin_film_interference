7b91166e0a26e2c79e194e6537560c4607b35ca3
george <george.duffett@icloud.com>
2022-11-06 19:32:17 +0000

    Change fresnel function definition locations

--- poc/main.c
+++ poc/main.c
@@ -2,49 +2,8 @@
 #include <complex.h>
 #include <tgmath.h>
 
+#include "fresnel.c"
 
-float complex calculate_senkrecht_reflection (
-    float complex *incident_wavevector_normal_component,
-    float complex *transmission_wavevector_normal_component
-) {
-    float complex numerator = (*incident_wavevector_normal_component) - (*transmission_wavevector_normal_component);
-    float complex denominator = (*incident_wavevector_normal_component) + (*transmission_wavevector_normal_component);
-    return numerator / denominator;
-}
-
-float complex calculate_senkrecht_transmission (
-    float complex *incident_wavevector_normal_component,
-    float complex *transmission_wavevector_normal_component
-) {
-    float complex numerator = 2 * (*incident_wavevector_normal_component);
-    float complex denominator = (*incident_wavevector_normal_component) + (*transmission_wavevector_normal_component);
-    return numerator / denominator;
-}
-
-float complex calculate_parallel_reflection(
-        float complex *incident_wavevector_normal_component,
-        float complex *transmission_wavevector_normal_component,
-        const float complex *incident_refractive_index,
-        const float complex *transmission_refractive_index
-) {
-        float complex numerator = (*incident_wavevector_normal_component) * pow(*transmission_refractive_index,2) - 
-            (*transmission_wavevector_normal_component) * pow(*incident_refractive_index,2);
-        float complex denominator = (*incident_wavevector_normal_component) * pow(*transmission_refractive_index,2) + 
-            *transmission_wavevector_normal_component * pow(*incident_refractive_index,2);
-        return numerator / denominator;
-}
-
-float complex calculate_parallel_transmission(
-        float complex *incident_wavevector_normal_component,
-        float complex *transmission_wavevector_normal_component,
-        const float complex *incident_refractive_index,
-        const float complex *transmission_refractive_index
-) {
-        float complex numerator = 2 * (*incident_wavevector_normal_component) * (*incident_refractive_index) * (*transmission_refractive_index);
-        float complex denominator = (*incident_wavevector_normal_component) * pow(*transmission_refractive_index,2) + 
-            (*transmission_wavevector_normal_component) * pow(*incident_refractive_index,2);
-        return numerator / denominator;
-}
 
 float complex calculate_film_reflection(
     float complex reflection_out_of,
