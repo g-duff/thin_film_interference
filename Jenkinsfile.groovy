b99aba063a844edea85786bebfbf42aa58c8d00c
george <george.duffett@icloud.com>
2023-03-10 16:42:22 +0000

    Add post-build notification

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,4 +20,33 @@ pipeline {
 		}
 	}
 
+	post {
+
+		success {
+			withCredentials([string(credentialsId: 'GitHubStatusToken', variable: 'TOKEN')]) {
+				sh '''curl -L \
+					-X POST \
+					"https://api.github.com/repos/g-duff/thin_film_interference/statuses/$GIT_COMMIT" \
+					-H "Accept: application/vnd.github+json" \
+					-H "X-GitHub-Api-Version: 2022-11-28" \
+					-H "Authorization: Bearer $TOKEN"\
+					-d '{"state":"success","context":"continuous-integration/jenkins"}'
+					'''
+			}
+		}
+
+		failure {
+			withCredentials([string(credentialsId: 'GitHubStatusToken', variable: 'TOKEN')]) {
+				sh '''curl -L \
+					-X POST \
+					"https://api.github.com/repos/g-duff/thin_film_interference/statuses/$GIT_COMMIT" \
+					-H "Accept: application/vnd.github+json" \
+					-H "X-GitHub-Api-Version: 2022-11-28" \
+					-H "Authorization: Bearer $TOKEN"\
+					-d '{"state":"failure","context":"continuous-integration/jenkins"}'
+				'''
+			}
+		}
+
+	}
 }
