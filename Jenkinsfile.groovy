f90627391887271856d766774f84eb4881c2ef36
george <george.duffett@icloud.com>
2023-03-12 12:46:11 +0000

    Add unstable state for linting

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,7 +10,13 @@ pipeline {
 		}
 		stage('Lint') {
 			steps {
-				sh "make lint"
+				script {
+					try {
+						sh "make lint"
+					} catch (error) {
+						unstable(message: "${STAGE_NAME} is unstable")
+					}
+				}
 			}
 		}
 		stage('Test') { 
@@ -36,6 +42,23 @@ pipeline {
 					-H "Authorization: Bearer $TOKEN"\
 					-d '{\
 						"state": "success", \
+						"context": "continuous-integration/jenkins", \
+						"target_url": "https://github.com/g-duff/Jenkins" \
+					}'
+					'''
+			}
+		}
+
+		unstable {
+			withCredentials([string(credentialsId: 'GitHubStatusToken', variable: 'TOKEN')]) {
+				sh '''curl -L \
+					-X POST \
+					"https://api.github.com/repos/g-duff/thin_film_interference/statuses/$GIT_COMMIT" \
+					-H "Accept: application/vnd.github+json" \
+					-H "X-GitHub-Api-Version: 2022-11-28" \
+					-H "Authorization: Bearer $TOKEN"\
+					-d '{\
+						"state": "error", \
 						"context": "continuous-integration/jenkins", \
 						"target_url": "https://github.com/g-duff/Jenkins" \
 					}'
