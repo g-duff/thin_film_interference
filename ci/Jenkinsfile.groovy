484af734161d930d76d471fba9e78789ae54afb8
george <george.duffett@icloud.com>
2023-07-09 21:45:41 +0100

    style: whitespace

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -1,62 +1,62 @@
 @Library('pipeline-lib') _
 
 pipeline {
-	
-	agent { label 'python' }
-	
-	stages {
-		stage('Install dependencies') {
-			steps {
-				sh "make dev_dependencies"
-			}
-		}
-		stage('Lint') {
-			steps {
-				script {
-					try {
-						sh "make lint"
-					} catch (error) {
-						unstable(message: "${STAGE_NAME} is unstable")
-					}
-				}
-			}
-		}
-		stage('Test') { 
-			steps {
-				script {
-					try {
-						sh "make test"
-					} catch (error) {
-						unstable(message: "${STAGE_NAME} is unstable")
-					}
-				}
-			}
-		}
-	}
+    
+    agent { label 'python' }
+    
+    stages {
+        stage('Install dependencies') {
+            steps {
+                sh "make dev_dependencies"
+            }
+        }
+        stage('Lint') {
+            steps {
+                script {
+                    try {
+                        sh "make lint"
+                    } catch (error) {
+                        unstable(message: "${STAGE_NAME} is unstable")
+                    }
+                }
+            }
+        }
+        stage('Test') { 
+            steps {
+                script {
+                    try {
+                        sh "make test"
+                    } catch (error) {
+                        unstable(message: "${STAGE_NAME} is unstable")
+                    }
+                }
+            }
+        }
+    }
 
-	post {
+    post {
 
-		success {
-			script {
-				notifyGitHubBuildStatus("thin_film_interference", "success")
-			}
-		}
+        success {
+            script {
+                notifyGitHubBuildStatus("thin_film_interference", "success")
+            }
+        }
 
-		unstable {
-			script {
-				notifyGitHubBuildStatus("thin_film_interference", "failure")
-			}
-		}
+        unstable {
+            script {
+                notifyGitHubBuildStatus("thin_film_interference", "failure")
+            }
+        }
 
-		failure {
-			script {
-				notifyGitHubBuildStatus("thin_film_interference", "error")
-			}
-		}
+        failure {
+            script {
+                notifyGitHubBuildStatus("thin_film_interference", "error")
+            }
+        }
 
-		cleanup {
-			sh "make clean"
-		}
+        cleanup {
+            sh "make clean"
+        }
 
-	}
+    }
 }
